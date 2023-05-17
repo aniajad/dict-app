363d76ef9ea7afb8191997549a6728be53077519
aniajad <116589679+aniajad@users.noreply.github.com>
2023-05-17 21:10:18 +0200

    Creating a search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
-      <button type="button" class="btn">
-        Base class
-      </button>
+      <div className="container">
+        <header>Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Anna Jadczyszyn</footer>
+      </div>
     </div>
   );
 }
