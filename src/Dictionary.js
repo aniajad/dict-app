f37b30f0cba11304e9ae40e1308765fad2168692
aniajad <116589679+aniajad@users.noreply.github.com>
2023-05-19 19:49:29 +0200

    added axios, connected to API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,9 +5,18 @@ import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiKey = "d6adb6d48b0afcb13103tf940oab4e26";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeywordChange(event) {
