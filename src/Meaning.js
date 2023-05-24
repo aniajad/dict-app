38645dcc3b6fc8c720da1410d59abf2c7e9fc703
aniajad <116589679+aniajad@users.noreply.github.com>
2023-05-24 21:04:49 +0200

    created new component Synonyms, moved the handling of synonyms to the new component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  let synonyms =
-    props.meaning.synonyms && props.meaning.synonyms.length > 0
-      ? props.meaning.synonyms.join(", ")
-      : null;
-
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
@@ -16,8 +12,7 @@ export default function Meaning(props) {
         <p className="example">Example: {props.meaning.example}</p>
       )}
       {!props.meaning.example && <div>No examples found</div>}
-      {synonyms && <span>Similar: {synonyms}</span>}
-      {!synonyms && <div>No synonyms found</div>}
+      <Synonyms synonyms={props.meaning.synonyms} />
       <div className="Meaning-separator"></div>
     </div>
   );
