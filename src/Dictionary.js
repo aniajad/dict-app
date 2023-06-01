223b07e117f19b0b67cd09644da1b09b9d1a9071
aniajad <116589679+aniajad@users.noreply.github.com>
2023-06-01 22:11:02 +0200

    adjusting footer font size

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -37,7 +37,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h3>What word would you like to look up?</h3>
+          <h3>Search for a word</h3>
           <form onSubmit={handleSubmit} className="input-group">
             <input
               type="search"
